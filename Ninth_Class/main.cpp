9ac94c230c3fc1b41bec177956d7de14fa1f83f9
ku-droid <52478527+ku-droid@users.noreply.github.com>
2020-10-02 12:17:21 +0545

    Function #4
    Description on Pass-by-reference and idea about how the function works with their examples.

---
+++ Ninth_Class/main.cpp
@@ -0,0 +1,109 @@
+// Pass-by-reference
+/*
+ *  Sometimes we want to be able to change actual parameters from within the function body.
+ *  inplace = True  :dataframe.drop(inplace = True)
+ *  in order to achieve this we need the location or address of the actual parameters.
+ *  Formal parameters will be an alias for the actual parameters.
+ */
+ 
+ 
+// Example -> 1:
+ 
+// #include<iostream>
+// using namespace std;
+// void scale_number(int &num);  //Prototype
+// int main()
+// {
+//     int number={1000};
+//     cout<<"Before :"<<number<<endl;
+//     cout<<"After :"
+//     scale_number(number);      //Function call
+//     cout<<number <<endl;       //100
+//     
+//     return 0;
+// }
+// 
+// void scale_number(int &num)    //Defination
+// {
+//     if(num>100)
+//         num=100;
+// }
+
+
+// Example -> 2:
+
+//#include<iostream>
+//using namespace std;
+//void swap(int &a,int &b);
+//int main()
+//{
+//    int x={10},y={20};
+//    cout<<x<<" "<<y<<endl;
+//    swap(x,y);
+//    cout<<x<<" "<<y<<endl;
+//    return 0;
+//}
+//
+//void swap(int &a,int &b)
+//{
+//    int temp =a;
+//    a=b;
+//    b=temp;
+//}
+
+// Example -> 3:
+//#include<vector>
+//#include<iostream>
+//using namespace std;
+//void print(vector<int> &v);
+//
+//int main()
+//{
+//    vector<int>data{1,2,3,4,5};
+//    print(data);
+//    return 0;
+//}
+//void print(vector <int> &v)
+//{
+//    int t=10;
+//    for (auto num:v)
+//        cout<<num+t<<endl;
+//    cout<<endl;
+//}
+
+
+// How the function works :
+
+/*
+ * Function use the "function call stack "
+ * ---> Analogous to a stack of books.
+ * ---> LIFO : Last in first out
+ * ---> Push and pop
+  
+ *  StackFRame or Activation Record(each time a funtion is called activation record is created and pushed in a stack )
+ *  When the function terminates we pop its activation record off the call stack .
+ *  A function must return control.
+ */
+ 
+// #include<iostream>
+// using namespace std;
+// void funct2(int &x,int y,int z)
+// {
+//     x+=y+z;
+// }
+// int funct1(int a,int b)
+// {
+//     int result={};
+//     result =a+b;
+//     funct2(result,a,b);
+//     return result;
+// }
+// int main()
+// {
+//     int x={10};
+//     int y={20};
+//     int z={};
+//     
+//     z=funct1(x,y);
+//     cout<<z<<endl;
+// }
